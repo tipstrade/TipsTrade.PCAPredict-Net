b6af06ed8a1d5a1953801c78541cbd49f517342b
oobayly <oobayly@yahoo.com>
2017-11-08 18:56:53 +0000

    Add configuration for unit tests

--- Tests/UnitTest.cs
+++ Tests/UnitTest.cs
@@ -1,9 +1,34 @@
+using Microsoft.Extensions.Configuration;
 using System;
-using System.Threading.Tasks;
-using Xunit;
 
 namespace TipsTrade.PCAPredict.Tests {
   public class UnitTest {
+    private static IConfiguration config;
+
+    /// <summary>Gets or sets the Test Configration.</summary>
+    private static IConfiguration Configuration {
+      get {
+        if (config == null) {
+          config = new ConfigurationBuilder()
+            .AddJsonFile("client-secrets.json")
+            .Build();
+        }
+
+        return config;
+      }
+    }
+
+    /// <summary>Creates an instance of the PCA Predict client.</summary>
+    private static PCAPredictClient CreateClient() {
+      var section = Configuration.GetSection("pca");
+      if (section == null) {
+        throw new Exception("A 'pca' configuration section is required.");
+      }
+
+      return new PCAPredictClient() {
+        ApiKey = section["ApiKey"],
+        GeocodeKey = section["GeocodeKey"]
+      };
     }
   }
 }
